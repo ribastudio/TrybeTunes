f0c51ebed56e2141b1ff72c1facb71a00088b1f1
Riba Jr / RSTD <rstd.creative@gmail.com>
2021-09-14 17:11:09 -0300

    Added initials files and starting the project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Album from './pages/Album';
+import NotFound from './pages/NotFound';
+import Favorites from './pages/Favorites';
+import Search from './pages/Search';
+import ProfileEdit from './pages/ProfileEdit';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <BrowserRouter>
+          <Route path="*">
+            <NotFound />
+          </Route>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="profile">
+            <Profile />
+          </Route>
+          <Route path="album/:id">
+            <Album />
+          </Route>
+          <Route path="favorites">
+            <Favorites />
+          </Route>
+          <Route path="search">
+            <Search />
+          </Route>
+          <Route path="profile/edit">
+            <ProfileEdit />
+          </Route>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
