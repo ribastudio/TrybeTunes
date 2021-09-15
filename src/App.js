c016a88019c11962f64577e41ee7f462d3dadc29
Riba Jr / RSTD <rstd.creative@gmail.com>
2021-09-14 21:00:58 -0300

    Question 1 resolved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Album from './pages/Album';
@@ -7,35 +7,40 @@ import NotFound from './pages/NotFound';
 import Favorites from './pages/Favorites';
 import Search from './pages/Search';
 import ProfileEdit from './pages/ProfileEdit';
+import Header from './components/Header';
+import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <BrowserRouter>
-          <Route path="*">
-            <NotFound />
-          </Route>
-          <Route exact path="/">
-            <Login />
-          </Route>
-          <Route path="profile">
-            <Profile />
-          </Route>
-          <Route path="album/:id">
-            <Album />
-          </Route>
-          <Route path="favorites">
-            <Favorites />
-          </Route>
-          <Route path="search">
-            <Search />
-          </Route>
-          <Route path="profile/edit">
-            <ProfileEdit />
-          </Route>
-        </BrowserRouter>
-      </div>
+      <BrowserRouter>
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route path="/album/:id">
+              <Album />
+            </Route>
+            <Route path="/favorites">
+              <Favorites />
+            </Route>
+            <Route exact path="/profile">
+              <Header />
+              <Profile />
+            </Route>
+            <Route path="/profile/edit">
+              <ProfileEdit />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </main>
+      </BrowserRouter>
     );
   }
 }
